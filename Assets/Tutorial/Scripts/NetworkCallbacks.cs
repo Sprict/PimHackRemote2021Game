33a6279011818f509dcdd97baec623051312ca8e
Sprict <raren0130@gmail.com>
2021-02-24 02:07:39 +0900

    Tutorialフォルダ追加

---
+++ Assets/Tutorial/Scripts/NetworkCallbacks.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[BoltGlobalBehaviour]　//この属性をつけたクラスをBoltが自動インスタンス化
+public class NetworkCallbacks : Bolt.GlobalEventListener {
+    public override void SceneLoadLocalDone (string scene) {
+
+        // randomize a position
+        var spawnPosition = new Vector3 (Random.Range (-16, 16), 0, Random.Range (-16, 16));
+
+        // instantiate cube
+        BoltNetworkUtils.Instantiate (BoltPrefabs.Cube, spawnPosition, Quaternion.identity);
+    }
+}
